04a0f08719d194e2a4339d684773dd6029037137
unknown <tmc7617@g.rit.edu>
2012-10-16 04:42:13 -0400

    Mockup for MakeQuiz

--- ASLTutor/src/com/handicapable/asltutor/MakeQuizActivity.java
+++ ASLTutor/src/com/handicapable/asltutor/MakeQuizActivity.java
@@ -7,19 +7,25 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 @TargetApi(11)
 public class MakeQuizActivity extends Activity {
 
+
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_make_quiz);
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
+		}	
 	}
-
+		
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_make_quiz, menu);
@@ -36,4 +42,9 @@ public class MakeQuizActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	public void saveQuiz(View view){
+		Toast toast = Toast.makeText(getApplicationContext(), "Quiz Saved!",
+				Toast.LENGTH_SHORT);
+		toast.show();
+	}
 }
